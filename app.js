126484dfe30ed7e73780d9b4087a95f94acce032
Abdullahsani23 <abdullahsani95@gmail.com>
2024-03-11 00:58:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// alert("hello")
+
+
+// // var name =prompt("enter your name")
+// // alert("hello " + name)
+// // var age =prompt("enter your age " + name)
+// // if(age <18){
+// //     alert("this site is not for you " + name )
+// // }
+// // else{
+// //     alert("welcome to 18+ page " + name)
+// // }
